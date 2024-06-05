5f8fa72cb0032607298189d4e2f87986fb5d45cb
Adam Scott <ascott.ca@gmail.com>
2024-06-05 16:40:30 -0400

    Add mobile compatible buttons

--- node_2d.gd
+++ node_2d.gd
@@ -9,5 +9,13 @@ func _process(delta: float) -> void:
 
 
 func _draw() -> void:
-	for i in range($SpinBox.value):
+	for i in range(%SpinBox.value):
 		draw_texture(texture, Vector2(randf() * 1200, randf() * 800), Color(1, 1, 1, 1))
+
+
+func _on_minus_button_pressed() -> void:
+	%SpinBox.value -= 100
+
+
+func _on_plus_button_pressed() -> void:
+	%SpinBox.value += 100
